129be25535497d9013afa66b60d15e1cf9b4ab2d
Macktzy <rakusama0@gmail.com>
2026-07-29 17:48:03 +0800

    fix: Update PWA icons

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,11 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
+            src: 'favicon.svg',
+            sizes: '192x192 512x512',
+            type: 'image/svg+xml'
+          },
+          {
             src: 'logo.jpg',
             sizes: '192x192',
             type: 'image/jpeg'
